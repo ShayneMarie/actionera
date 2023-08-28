75eff4849b8fcb7d6d957d06a6d641f12aad5c46
ShayneMarie <shayne.marie.ulep@gmail.com>
2023-08-28 16:36:04 +0800

    Update test data for superadmin and creator

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -2,12 +2,14 @@ describe('Login', () => {
   it('Successful login as creator', () => {
     cy.visit('/');
     cy.login('creator');
-    cy.contains('Josephine Neric');
+    cy.contains('shayne member');
+    cy.get('img[alt="Admin"]').should('not.exist');
   })
 
   it('Successful login as super admin', () => {
     cy.visit('/');
     cy.login('superAdmin');
-    cy.contains('Shayne');
+    cy.contains('Demo User');
+    cy.get('img[alt="Admin"]').should('exist');
   })
 })
